363ce8050b96413cd001086c4b420648c78ae790
Suman Atta <sumanatta634@gmail.com>
2023-09-12 07:34:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <iframe src="https://giphy.com/embed/Mah9dFWo1WZX0WM62Q" width="200" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/pudgypenguins-building-build-brick-Mah9dFWo1WZX0WM62Q"></a></p>
+        <h1>Sorry! This Website is under Construction!!!</h1>
+        <p>See you soon🫠</p>
       </header>
     </div>
   );
