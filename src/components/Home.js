c715538da903720383601159325ff01930cabac3
SUMAN ATTA <92137683+ss357@users.noreply.github.com>
2023-10-06 09:17:38 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,14 @@ import pic from '../assests/IMG_20200830_162755~3.jpg'
 const Home = ({id}) => {
   return (
     <div name='home' className="h-screen w-full px-8 text-white flex flex-col justify-center  mx-auto md:px-20">
-    <div className="hidden md:block xl:block absolute right-20 top-42 border-cyan-700 md:w-52 xl:w-80 border-2 md:shadow-md xl:shadow-2xl rounded-lg overflow-hidden aspect-w-2 aspect-h-1 shadow-slate-50 border-r-8 border-b-8 z-0">
+   <div className="hidden lg:block absolute right-20 top-42 border-cyan-700 lg:w-52 xl:w-80 border-2 lg:shadow-md xl:shadow-2xl rounded-lg overflow-hidden aspect-w-2 aspect-h-1 shadow-slate-50 border-r-8 border-b-8 z-0">
   <img
     src={pic}
     alt=""
     className="h-full w-full object-conatain shadow-md"
   />
 </div>
+
 
       <p className="text-cyan-400 font-mono text-xl">Hi my name is</p>
       <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
